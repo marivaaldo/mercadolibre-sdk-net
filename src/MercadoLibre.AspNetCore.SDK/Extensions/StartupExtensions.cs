242b5d15759eba88b2ad52ad25e47a755d92c358
Marivaldo Júnior <desenvolvedor@bibipecas.com.br>
2020-01-23 11:02:49 -0300

    Added test server application

---
+++ src/MercadoLibre.AspNetCore.SDK/Extensions/StartupExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MercadoLibre.AspNetCore.SDK.Extensions
+{
+    public static class StartupExtensions
+    {
+        public static IServiceCollection AddMercadoLibre(this IServiceCollection services, long clientId, string clientSecret)
+        {
+            services.AddTransient<Meli>(o => new Meli(clientId, clientSecret));
+
+            return services;
+        }
+    }
+}
---
+++ src/MercadoLibre.AspNetCore.TestServer/Controllers/MercadoLibreController.cs
@@ -0,0 +1,53 @@
+﻿using MercadoLibre.AspNetCore.SDK;
+using MercadoLibre.AspNetCore.SDK.Exceptions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MercadoLibre.AspNetCore.TestServer.Controllers
+{
+    [ApiController]
+    [Route("ml")]
+    public class MercadoLibreController : Controller
+    {
+        private Dictionary<string, SDK.Models.AuthorizeToken> _Tokens = new Dictionary<string, SDK.Models.AuthorizeToken>();
+
+        private Meli Meli { get; }
+
+        public MercadoLibreController(Meli meli)
+        {
+            Meli = meli;
+        }
+
+        [HttpGet("")]
+        public async Task<IActionResult> Index()
+        {
+            return Redirect(Meli.GetAuthUrl(AuthUrls.MLB, "https://localhost:5001/ml/auth"));
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Auth(string code)
+        {
+            try
+            {
+                await Meli.AuthorizeAsync(code, "https://localhost:5001/ml/auth");
+
+                _Tokens[Meli.AuthorizeToken.UserId] = Meli.AuthorizeToken;
+
+                return View(Meli.AuthorizeToken);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpGet()]
+        public IActionResult Notifications()
+        {
+            return Ok();
+        }
+    }
+}
